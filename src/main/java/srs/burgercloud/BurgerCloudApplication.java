56f1e1e552dd087b1cc5f016eccddc5a501874c6
Shubham <shubhamshinde0803@gmail.com>
2023-04-22 10:38:46 +0530

    Revert "implemented view controller implementation" reverting view controllers implementation This reverts commit 4bd13b989a7586fc89840a458bb70fc0929fb078.

--- src/main/java/srs/burgercloud/BurgerCloudApplication.java
+++ src/main/java/srs/burgercloud/BurgerCloudApplication.java
@@ -2,20 +2,12 @@ package srs.burgercloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BurgerCloudApplication implements WebMvcConfigurer {
+public class BurgerCloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BurgerCloudApplication.class, args);
-	}
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry){
-		System.out.println("implemented home controller with view controllers");
-		registry.addViewController("/").setViewName("home");
 	}
 
 }
