cc88373c7b414441e766320506c0945fd583ed7c
Shubham Shinde <sshinde@cdac.in>
2023-05-10 13:58:20 +0530

    added updated security properties

--- src/main/java/srs/example/burgerCloudApplication/security/SecurityConfig.java
+++ src/main/java/srs/example/burgerCloudApplication/security/SecurityConfig.java
@@ -5,59 +5,70 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import srs.example.burgerCloudApplication.data.UserRepository;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig{
 
-//    private final UserDetailsService userDetailsService;
     @Bean
     public PasswordEncoder encoder(){
         return new BCryptPasswordEncoder();
     }
 
-//    @Autowired
-//    private UserDetailsService userDetailsService;
+@Autowired
+    UserRepository userRepo;
 
     @Bean
-    public AuthenticationManager configure(AuthenticationConfiguration authenticationConfiguration) throws Exception{
-        return authenticationConfiguration.getAuthenticationManager();
+    UserDetailsService userDetailsService() throws Exception{
+
+        return  username-> userRepo.findByUsername(username);
+
     }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-//        auth
-//                .userDetailsService(userDetailsService)
-//                .passwordEncoder(encoder());
-//    }
+    @Bean
+    AuthenticationProvider authenticationProvider() throws Exception{
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/design", "/orders").hasRole("USER")
-                .requestMatchers("/", "/**").permitAll()
-//                .anyRequest()
-//                .authenticated()
-                .and()
                 .formLogin()
                 .loginPage("/login")
                 .defaultSuccessUrl("/design")
                 .and()
-//                .logout()
-//                .logoutSuccessUrl("/")
-//                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/design", "/orders").hasRole("USER")
+                .requestMatchers("/", "/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
 //                .sessionManagement()
 //                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 //                .and()
